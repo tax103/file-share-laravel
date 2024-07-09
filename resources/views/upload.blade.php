8504f55ac3301dbc55a652f749662ebacc2e4795
tax103 <wax103mg@gmail.com>
2024-07-09 20:14:36 +0900

    環境構築

---
+++ resources/views/upload.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>課題</title>
+        <style></style>
+    </head>
+    <body class="antialiased">
+        あいうえお
+    </body>
+
+</html>
+        
